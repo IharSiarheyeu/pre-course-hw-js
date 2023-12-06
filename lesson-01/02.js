48e965f06b42ac1a7f75979a7302662f4249bcb9
github-igor4093 <igor4093@gmail.com>
2023-12-06 11:02:20 +0300

    noComments

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,8 +1,3 @@
-/* Создай рандомное число при помощи  
-let number1 = Math.floor(Math.random() * 100)
- а затем проверь их выводя в консоль: «[вставь сюда полученное число] :Это четное число» или «[вставь сюда полученное число] 
- :Это нечетное число». С каждой перезагрузкой страницы у тебя будет новое число.​// code
- */
 
  let number1 = Math.floor(Math.random() * 100);
  
@@ -11,23 +6,6 @@ let number1 = Math.floor(Math.random() * 100)
   }
   else {console.log('[',number1,']', ':Это нечетное число');
 };
-
-
-
-// code */
-
-/* Имея в распоряжении только:
-​let number = 1;
-и используя лишь
---number   number--  ++number   number++
-Получи в консоли 1,3,3,1 
-Нужно вызвать консоль ЧЕТЫРЕ раза
-Пример:
-console.log(number**); 
-console.log(**number); 
-console.log(number**); 
-console.log(**number); 
- */
 
 let number = 1;
 console.log(number++);
